767f0c849ccde0e03c1f4f1df3fca158b70de803
JT Archie <jtarchie@gmail.com>
2019-02-10 11:19:35 -0700

    objectify

--- lib/nimble.rb
+++ lib/nimble.rb
@@ -2,13 +2,48 @@
 
 module Nimble
   def ascii_char(ranges)
-    Machine.new(lambda { |bytes, accum|
+    AsciiChar.new(ranges)
+  end
+
+  class Machine
+    def |(other)
+      Concat.new([self, other])
+    end
+  end
+
+  class Concat < Machine
+    def initialize(machines)
+      @machines = machines
+    end
+
+    def call(bytes, accum = [])
+      leftovers = @machines.reduce(bytes) do |b, machine|
+        status, accum, leftovers = machine.call(b, accum)
+
+        case status
+        when :ok
+          leftovers
+        when :error
+          return :error, accum, leftovers
+        end
+      end
+
+      return :ok, accum, leftovers
+    end
+  end
+
+  class AsciiChar < Machine
+    def initialize(ranges)
+      @ranges = ranges
+    end
+
+    def call(bytes, accum = [])
       char = bytes[0]
       accum.push char
 
-      return :ok, accum, bytes[1..-1] if ranges.empty?
+      return :ok, accum, bytes[1..-1] if @ranges.empty?
 
-      ranges.each do |range|
+      @ranges.each do |range|
         case range
         when Range
           return :ok, accum, bytes[1..-1] if range.include?(char)
@@ -21,34 +56,6 @@ module Nimble
 
       accum.pop
       return :error, accum, bytes
-    })
-  end
-
-  class Machine
-    def initialize(block)
-      @block = block
-    end
-
-    def call(bytes, accum = [])
-      @block.call(bytes, accum)
-    end
-
-    def |(other)
-      machines = [self, other]
-      Machine.new(lambda { |bytes, accum|
-        leftovers = machines.reduce(bytes) do |b, machine|
-          status, accum, leftovers = machine.call(b, accum)
-
-          case status
-          when :ok
-            leftovers
-          when :error
-            return :error, accum, leftovers
-          end
-        end
-
-        return :ok, accum, leftovers
-      })
     end
   end
 end
